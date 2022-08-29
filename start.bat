dedb0e52ba9c2d8e104c05d4a43fda0f162cb9fb
İbrahim YÖNDEMLİ <ibrahim-y@tuta.io>
2022-08-29 21:40:43 +0300

    1.19.2

--- start.bat
+++
@@ -1,3 +0,0 @@
-@echo off
-"C:\Program Files\Java\jdk-17.0.1\bin\java.exe" -Xms120M -Xmx2G -XX:+UseG1GC -jar paper.jar nogui
-pause
--- start.sh
+++
@@ -1,1 +0,0 @@
-java -Xms120M -Xmx2G -XX:+UseG1GC -jar purpur.jar nogui
